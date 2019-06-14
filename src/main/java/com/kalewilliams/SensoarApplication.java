08a940e8c8977e36b69b6d1e38b700583c08f97f
Kale <kale.palani@gmail.com>
2019-06-14 19:11:47 -0400

    Initial Commit: beginning of data files

---
+++ src/main/java/com/kalewilliams/SensoarApplication.java
@@ -0,0 +1,13 @@
+package com.kalewilliams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensoarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensoarApplication.class, args);
+	}
+
+}
